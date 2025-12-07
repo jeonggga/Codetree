e18afbe930e6d00c61601f9f5712261ade364160
jeonggga <gyj_802@naver.com>
2025-12-07 18:54:16 +0900

    Title: 비교 연산, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251207/비교 연산/comparison-operator.py
@@ -0,0 +1,35 @@
+nums = input()
+num = nums.split()
+
+a = int(num[0])
+b = int(num[1])
+
+if a >= b :
+    print(1)
+else :
+    print(0)
+
+if a > b :
+    print(1)
+else :
+    print(0)
+
+if b >= a :
+    print(1)
+else :
+    print(0)
+
+if b > a :
+    print(1)
+else :
+    print(0)
+
+if a == b :
+    print(1)
+else :
+    print(0)
+    
+if a != b :
+    print(1)
+else :
+    print(0)
