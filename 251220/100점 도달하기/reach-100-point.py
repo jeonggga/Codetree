7cc61b0029b14a6c4990d002456e81fd19b983d9
jeonggga <gyj_802@naver.com>
2025-12-20 13:19:28 +0900

    Title: 100점 도달하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251220/100점 도달하기/reach-100-point.py
+++ 251220/100점 도달하기/reach-100-point.py
@@ -7,31 +7,61 @@ num_list = []
 # 입력받은 숫자를 리스트에 처음으로 추가
 num_list.append(num)
 
+# if문에서 나온 값을 저장하기 위한 빈 리스트
+result = []
+
 # 리스트의 마지막 값이 100 이하인 동안 반복
 while num_list[-1] <= 100:
 
-    # 점수가 90 이상이면 A 출력
+    # 점수가 90 이상이면 "A" 문자열을 result 리스트에 추가
     if num_list[-1] >= 90:
-        print("A", end=" ")
+        result.append("A")
 
-    # 점수가 80 이상이면 B 출력
     elif num_list[-1] >= 80:
-        print("B", end=" ")
+        result.append("B")
 
-    # 점수가 70 이상이면 C 출력
     elif num_list[-1] >= 70:
-        print("C", end=" ")
+        result.append("C")
 
-    # 점수가 60 이상이면 D 출력
     elif num_list[-1] >= 60:
-        print("D", end=" ")
+        result.append("D")
 
-    # 그 외(60 미만)는 F 출력
     else:
-        print("F", end=" ")
+        result.append("F")
 
-    # 현재 마지막 숫자에 1을 더함
+    # num_list 리스트의 마지막 숫자에 +1 하고 num_list에 추가
     num_list[-1] += 1
-
-    # 증가된 숫자를 리스트에 다시 추가
     num_list.append(num_list[-1])
+    
+print(" ".join(result))
+
+
+# --- 다른 방법 ---
+# # 리스트의 마지막 값이 100 이하인 동안 반복
+# while num_list[-1] <= 100:
+
+#     # 점수가 90 이상이면 A 출력
+#     if num_list[-1] >= 90:
+#         print("A", end=" ")
+
+#     # 점수가 80 이상이면 B 출력
+#     elif num_list[-1] >= 80:
+#         print("B", end=" ")
+
+#     # 점수가 70 이상이면 C 출력
+#     elif num_list[-1] >= 70:
+#         print("C", end=" ")
+
+#     # 점수가 60 이상이면 D 출력
+#     elif num_list[-1] >= 60:
+#         print("D", end=" ")
+
+#     # 그 외(60 미만)는 F 출력
+#     else:
+#         print("F", end=" ")
+
+#     # 현재 마지막 숫자에 1을 더함
+#     num_list[-1] += 1
+
+#     # 증가된 숫자를 리스트에 다시 추가
+#     num_list.append(num_list[-1])
